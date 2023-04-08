c35e5b727add63ff4eda2bbab4fef55d42bc2700
pavel <tomashaytis@gmail.com>
2023-04-08 15:51:31 +0400

    Подключил логирование. Добавил функцию, генерирующую ассиметричные ключи (generate_asymmetric_keys()) и функцию, сериализующую их в файлы (save_asymmetric_keys()).

--- key_generation.py
+++ key_generation.py
@@ -1,5 +1,52 @@
 import os
+import logging
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+logger = logging.getLogger()
+logger.setLevel('INFO')
 
 
 def generate_symmetric_key(length: int) -> bytes:
-    return os.urandom(length)
+    key = os.urandom(length)
+    logging.info(f' symmetric key generated')
+    return key
+
+
+def generate_asymmetric_keys() -> tuple:
+    keys = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+    private_key = keys
+    public_key = keys.public_key()
+    logging.info(f' asymmetric keys generated')
+    return private_key, public_key
+
+
+def save_asymmetric_keys(private_key, public_key, private_pem: str, public_pem: str) -> None:
+    try:
+        with open(private_pem, 'wb') as private_out:
+            private_out.write(private_key.private_bytes(encoding=serialization.Encoding.PEM,
+                                                        format=serialization.PrivateFormat.TraditionalOpenSSL,
+                                                        encryption_algorithm=serialization.NoEncryption()))
+        logging.info(f' private key saved to {private_pem}')
+    except OSError as err:
+        logging.warning(f' private key was not saved to file {private_pem}\n{err}')
+    try:
+        with open(public_pem, 'wb') as public_out:
+            public_out.write(public_key.public_bytes(encoding=serialization.Encoding.PEM,
+                                                     format=serialization.PublicFormat.SubjectPublicKeyInfo))
+        logging.info(f' public key saved to {public_pem}')
+    except OSError as err:
+        logging.warning(f' public key was not saved to file {public_pem}\n{err}')
+
+
+
+def save_symmetric_key(key: bytes, file_name: str) -> None:
+    try:
+        with open(file_name, 'wb') as key_file:
+            key_file.write(key)
+        logging.info(f' symmetric key saved to {file_name}')
+    except OSError as err:
+        logging.warning(f' symmetric key was not saved to file {file_name}\n{err}')
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import os
 import argparse
-from key_generation import generate_symmetric_key
+import json
+from key_generation import generate_symmetric_key, generate_asymmetric_keys, save_asymmetric_keys, save_symmetric_key
+SETTINGS_FILE = os.path.join('files', 'settings.json')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -9,8 +12,12 @@ if __name__ == '__main__':
     group.add_argument('-enc', '--encryption', help='Запускает режим шифрования')
     group.add_argument('-dec', '--decryption', help='Запускает режим дешифрования')
     args = parser.parse_args()
+    with open(SETTINGS_FILE) as json_file:
+        settings = json.load(json_file)
     if args.generation is not None:
-        print(generate_symmetric_key(args.generation))
+        symmetric_key = generate_symmetric_key(args.generation)
+        private_key, public_key = generate_asymmetric_keys()
+        save_asymmetric_keys(private_key, public_key, settings['secret_key'], settings['public_key'])
     elif args.encryption is not None:
         pass
     else:
